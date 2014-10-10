76b10b02e4a0f22f9af591c2ea697dfa06949516
Conor Smyth <cnrsmyth@gmail.com>
2014-10-10 17:02:08 +0100

    Lab 2 completed. Needs formatting.

--- exercises.hs
+++ exercises.hs
@@ -1,10 +1,17 @@
-diff :: Int -> Int -> Int 
-diff x y = abs (x-y)
+-- Lab 2 --
+
+-- Questions 1 and 3
 
 triangleArea :: Float -> Float -> Float -> Float
-triangleArea a b c = 
-	let s = (a + b + c) / 2
-	in sqrt(s*(s-a)*(s-b)*(s-c))
+triangleArea a b c
+	| a + b < c = error "Not a triangle!"
+	| b + c < a = error "Not a triangle!"
+	| a + c < b = error "Not a triangle!"
+	| otherwise =
+		let s = (a + b + c) / 2
+		in sqrt(s*(s-a)*(s-b)*(s-c))
+
+-- Question 2
 
 isSum :: Int -> Int -> Int -> Bool
 isSum x y z 
@@ -12,3 +19,8 @@ isSum x y z
 	| x + z == y = True
 	| z + y == x = True
 	| otherwise  = False
+
+-- Lab 1 --
+
+diff :: Int -> Int -> Int 
+diff x y = abs (x-y)
