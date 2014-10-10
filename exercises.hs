b44ee4683aba4c828e7279b193bdc490e707b324
Conor Smyth <cnrsmyth@gmail.com>
2014-10-10 16:49:00 +0100

    Initial commit

---
+++ exercises.hs
@@ -0,0 +1,12 @@
+diff :: Int -> Int -> Int 
+diff x y = abs (x-y)
+
+triangleArea :: Float -> Float -> Float -> Float
+triangleArea a b c = 
+	let s = (a + b + c) / 2
+	in sqrt(s*(s-a)*(s-b)*(s-c))
+
+isSum :: Int -> Int -> Int -> Bool
+isSum x y z =
+
+
